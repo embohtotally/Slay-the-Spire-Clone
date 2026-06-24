d76d3de3bf7a14c88eeff34cb13cb675f4a76384
embohtotally <emboh166@gmail.com>
2026-06-24 20:47:00 +0700

    Update Card

--- Assets/_Project/Scripts/Systems/CardSystem.cs
+++ Assets/_Project/Scripts/Systems/CardSystem.cs
@@ -102,7 +102,7 @@ public class CardSystem : Singleton<CardSystem>
     {
         hand.Remove(playCardGA.Card);
         CardView cardView = handView.RemoveCard(playCardGA.Card);
-        yield return DiscardCard(cardView);
+        yield return PlayAndDiscardCard(cardView);
 
         SpendMana(playCardGA);
         DoManualTargetEffect(playCardGA);
@@ -134,6 +134,25 @@ public class CardSystem : Singleton<CardSystem>
     #endregion
 
     #region Helpers
+    private IEnumerator PlayAndDiscardCard(CardView cardView)
+    {
+        discardPile.Add(cardView.Card);
+        UpdatePileTexts();
+
+        cardView.transform.DOMove(new Vector3(0, 0, -1f), doTweenMoveDuration);
+        cardView.transform.DORotate(Vector3.zero, doTweenMoveDuration);
+        Tween scaleTween = cardView.transform.DOScale(Vector3.one * 1.5f, doTweenScaleDuration);
+        yield return scaleTween.WaitForCompletion();
+
+        yield return new WaitForSeconds(0.2f);
+
+        cardView.transform.DOScale(Vector3.zero, doTweenScaleDuration);
+        Tween moveTween = cardView.transform.DOMove(discardPilePoint.position, doTweenMoveDuration);
+        yield return moveTween.WaitForCompletion();
+
+        Destroy(cardView.gameObject);
+    }
+
     private IEnumerator DiscardCard(CardView cardView)
     {
         discardPile.Add(cardView.Card);
