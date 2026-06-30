55ddcd6723d67d2ae7f047b6cc65657675cf7a64
embohtotally <emboh166@gmail.com>
2026-06-30 21:24:55 +0700

    Update CombatantHurtState.cs

--- Assets/_Project/Scripts/StateMachine/CombatantHurtState.cs
+++ Assets/_Project/Scripts/StateMachine/CombatantHurtState.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CombatantHurtState : CombatantState
 {
     private float shakeDuration;
+    private float shakeStrength;
     private float timer;
     private bool isComplete;
 
