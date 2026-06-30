7ae9a110d938ff3d9d76591aaff6257d1c806c9c
embohtotally <emboh166@gmail.com>
2026-06-30 21:00:12 +0700

    Update CombatantHurtState.cs

--- Assets/_Project/Scripts/StateMachine/CombatantHurtState.cs
+++ Assets/_Project/Scripts/StateMachine/CombatantHurtState.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class CombatantHurtState : CombatantState
 {
     private float shakeDuration;
-    private float shakeStrength;
+    private float timer;
+    private bool isComplete;
 
     public CombatantHurtState(CombatantView combatant, float shakeDuration, float shakeStrength, Action onComplete = null) 
         : base(combatant, onComplete) 
@@ -18,10 +19,39 @@ public class CombatantHurtState : CombatantState
 
     public override void Enter()
     {
-        combatant.transform.DOShakePosition(shakeDuration, shakeStrength).OnComplete(() =>
+        timer = 0f;
+        isComplete = false;
+
+        if (combatant.Animator != null)
         {
-            onComplete?.Invoke();
-            combatant.StateMachine.ChangeState(new CombatantIdleState(combatant));
-        });
+            combatant.Animator.SetTrigger("Hurt");
+        }
+        else
+        {
+            combatant.transform.DOShakePosition(shakeDuration, shakeStrength).OnComplete(CompleteHurt);
+        }
+    }
+
+    public override void Update()
+    {
+        if (isComplete) return;
+
+        if (combatant.Animator != null)
+        {
+            timer += Time.deltaTime;
+            if (timer >= shakeDuration)
+            {
+                CompleteHurt();
+            }
+        }
+    }
+
+    private void CompleteHurt()
+    {
+        if (isComplete) return;
+        isComplete = true;
+
+        onComplete?.Invoke();
+        combatant.StateMachine.ChangeState(new CombatantIdleState(combatant));
     }
 }
